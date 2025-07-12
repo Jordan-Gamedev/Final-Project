77a9882455aecd303695b6470ab5b38d540cc647
Jordan-Gamedev <jordan.frazer@outlook.com>
2025-07-11 22:40:06 -0500

    main func

--- Scripts/main.py
+++ Scripts/main.py
@@ -12,95 +12,100 @@ from hopping_bug import *
 from player import *
 from sprite import *
 
-######################## set window properties ########################
+def main():
 
-# delete data files in case the program crashed last time and the corrupted files are still there
-while os.path.exists("Data"):
-    shutil.rmtree("Data", ignore_errors=True)
+    ######################## set window properties ########################
 
-# get currently used monitor
-current_monitor = get_current_monitor()
+    # delete data files in case the program crashed last time and the corrupted files are still there
+    while os.path.exists("Data"):
+        shutil.rmtree("Data", ignore_errors=True)
 
-# create maximized window
-set_config_flags(FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_ALWAYS_RUN | FLAG_WINDOW_RESIZABLE)
-init_window(1920, 1080, 'Game')
-maximize_window()
-hide_cursor()
+    # get currently used monitor
+    current_monitor = get_current_monitor()
 
-# set the maximum frame rate to the maximum refresh rate of the monitor 
-set_target_fps(get_monitor_refresh_rate(current_monitor))
+    # create maximized window
+    set_config_flags(FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_ALWAYS_RUN | FLAG_WINDOW_RESIZABLE)
+    init_window(1920, 1080, 'Game')
+    maximize_window()
+    hide_cursor()
 
-############################ import assets ############################
+    # set the maximum frame rate to the maximum refresh rate of the monitor 
+    set_target_fps(get_monitor_refresh_rate(current_monitor))
 
-os.makedirs("Data", exist_ok=True)
-open("Data\Shared_Main_Process_Sprite_Data.txt", "w").close()
+    ############################ import assets ############################
 
-player_textures_paths = ["Assets\Bat_1.png", "Assets\Bat_2.png"]
-player_loaded_textures = [load_texture(player_textures_paths[0]), load_texture(player_textures_paths[1])]
+    os.makedirs("Data", exist_ok=True)
+    open("Data\Shared_Main_Process_Sprite_Data.txt", "w").close()
 
-cursor_textures_paths = ["Assets\Cursor_Idle_1.png", "Assets\Cursor_Idle_2.png", "Assets\Cursor_Idle_3.png"]
-cursor_loaded_textures = [load_texture(cursor_textures_paths[0]), load_texture(cursor_textures_paths[1]), load_texture(cursor_textures_paths[2])]
+    player_textures_paths = ["Assets\Bat_1.png", "Assets\Bat_2.png"]
+    player_loaded_textures = [load_texture(player_textures_paths[0]), load_texture(player_textures_paths[1])]
 
-grass_textures_paths = ["Assets\Grass_1.png"]
-grass_loaded_textures = [load_texture(grass_textures_paths[0])]
+    cursor_textures_paths = ["Assets\Cursor_Idle_1.png", "Assets\Cursor_Idle_2.png", "Assets\Cursor_Idle_3.png"]
+    cursor_loaded_textures = [load_texture(cursor_textures_paths[0]), load_texture(cursor_textures_paths[1]), load_texture(cursor_textures_paths[2])]
 
-gnat_textures_paths = ["Assets\Gnat_1.png", "Assets\Gnat_2.png"]
+    grass_textures_paths = ["Assets\Grass_1.png"]
+    grass_loaded_textures = [load_texture(grass_textures_paths[0])]
 
-############################## game loop ##############################
+    gnat_textures_paths = ["Assets\Gnat_1.png", "Assets\Gnat_2.png"]
 
-player = Player(player_textures_paths, player_loaded_textures, 10.0, Vector2(), rot = 0.0, scale=4.9)
-player.speed = 1000
+    ############################## game loop ##############################
 
-cursor = Cursor(cursor_textures_paths, cursor_loaded_textures, 5.0, get_mouse_position())
+    player = Player(player_textures_paths, player_loaded_textures, 10.0, Vector2(), rot = 0.0, scale=4.9)
+    player.speed = 1000
 
-grass = Sprite(grass_textures_paths, grass_loaded_textures, 0.0, Vector2(0, get_monitor_height(current_monitor) - (27 * 5)), 0.0, 2.5)
+    cursor = Cursor(cursor_textures_paths, cursor_loaded_textures, 5.0, get_mouse_position())
 
-spawner = SpawnBugs(max_capacity=12, spawn_rate=3, fly_tex_paths=gnat_textures_paths, hopper_tex_paths=gnat_textures_paths, crawler_tex_paths=gnat_textures_paths)
+    grass = Sprite(grass_textures_paths, grass_loaded_textures, 0.0, Vector2(0, get_monitor_height(current_monitor) - (27 * 5)), 0.0, 2.5)
 
-process = subprocess.Popen(["python", "Scripts\sub.py"])
+    spawner = SpawnBugs(max_capacity=12, spawn_rate=3, fly_tex_paths=gnat_textures_paths, hopper_tex_paths=gnat_textures_paths, crawler_tex_paths=gnat_textures_paths)
 
-while not window_should_close():
+    process = subprocess.Popen(["python", "Scripts\sub.py"])
 
-    # updating
+    while not window_should_close():
 
-    delta_time = get_frame_time()
+        # updating
 
-    spawner.update(delta_time)
+        delta_time = get_frame_time()
 
-    for sprite in Sprite.all_sprites:
-        sprite.update(delta_time)
+        spawner.update(delta_time)
+
+        for sprite in Sprite.all_sprites:
+            sprite.update(delta_time)
     
-    # drawing
+        # drawing
     
-    begin_drawing()
+        begin_drawing()
 
-    clear_background(Color(0, 0, 0, 0))
+        clear_background(Color(0, 0, 0, 0))
     
-    draw_text("Press 'Esc' to close game", get_monitor_width(current_monitor) // 2 - 350, int(get_monitor_height(current_monitor) * 0.05), 50, WHITE)
+        draw_text("Press 'Esc' to close game", get_monitor_width(current_monitor) // 2 - 350, int(get_monitor_height(current_monitor) * 0.05), 50, WHITE)
 
-    for sprite in Sprite.all_sprites:
-        sprite.render()
+        for sprite in Sprite.all_sprites:
+            sprite.render()
     
-    draw_fps(0, 0)
+        draw_fps(0, 0)
     
-    end_drawing()
+        end_drawing()
 
-    # sync visuals to subprocesses
-    file = open("Data\Shared_Main_Process_Sprite_Data.txt", "w")
-    file.truncate()
+        # sync visuals to subprocesses
+        file = open("Data\Shared_Main_Process_Sprite_Data.txt", "w")
+        file.truncate()
     
-    new_file_contents = f"{player.get_current_texture_path()},{player.pos.x:.2f},{player.pos.y:.2f},{player.rot:.2f},{player.scale:.2f}\n"
+        new_file_contents = f"{player.get_current_texture_path()},{player.pos.x:.2f},{player.pos.y:.2f},{player.rot:.2f},{player.scale:.2f}\n"
 
-    little_bug:Bug = None
-    for little_bug in Bug.all_bugs:
-        new_file_contents += f"{little_bug.get_current_texture_path()},{little_bug.pos.x:.2f},{little_bug.pos.y:.2f},{little_bug.rot:.2f},{little_bug.scale:.2f}\n"
+        little_bug:Bug = None
+        for little_bug in Bug.all_bugs:
+            new_file_contents += f"{little_bug.get_current_texture_path()},{little_bug.pos.x:.2f},{little_bug.pos.y:.2f},{little_bug.rot:.2f},{little_bug.scale:.2f}\n"
 
-    file.write(new_file_contents)
-    file.close()
+        file.write(new_file_contents)
+        file.close()
 
-process.kill()
+    process.kill()
 
-while os.path.exists("Data"):
-    shutil.rmtree("Data", ignore_errors=True)
+    while os.path.exists("Data"):
+        shutil.rmtree("Data", ignore_errors=True)
 
-close_window()
+    close_window()
+
+if __name__ == "__main__":
+    main()
